9597c9e7b836aedb3362d13b12c881719fc402d5
Nathan Cassereau <nathan@cassereau.fr>
2025-03-19 17:46:23 +0000

    First draft of Markdown Streamer

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+import LlmModal from "$lib/LLMModal.svelte";
+
+let displayLLM: boolean = $state(false);
+
+</script>
+
+<button onclick={() => displayLLM = true}>Show LLM</button>
+
+{#if displayLLM}
+    <LlmModal onclick={() => displayLLM = false}/>
+{/if}
