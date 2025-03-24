f746e36879178f24c4d4470bfe2fab20aba872af
Nathan Cassereau <nathan@cassereau.fr>
2025-03-24 17:37:38 +0000

    Added my sexy face as a button for the llm modal

---
+++ src/lib/assistant-button.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+
+import { fade } from "svelte/transition";
+
+let { onclick }: { onclick: () => void} = $props();
+
+</script>
+
+<div
+    class="llm-button-wrapper"
+    in:fade={{ duration: 200 }}
+    out:fade={{ duration: 200 }}
+>
+    <div class="llm-button-gradient"></div>
+    <picture
+        {onclick}
+        role="button"
+        tabindex="0"
+        onkeypress={ev => ev.key === "Enter" && onclick()}
+    >
+        <source srcset="/moi.webp" type="image/webp">
+        <img
+            class="llm-button-img"
+            src="/moi.jpg"
+            alt="LLM assistant"
+            loading="lazy" />
+    </picture>
+</div>
+
+
+<style>
+
+.llm-button-wrapper {
+    position: fixed;
+    bottom: 40px;
+    right: 40px;
+    width: 66px;
+    height: 66px;
+}
+
+.llm-button-wrapper:hover {
+    cursor: pointer;
+}
+
+.llm-button-gradient {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: linear-gradient(45deg, #ff00cc, #3333ff, #00ccff);
+    animation: rotate 4s linear infinite;
+}
+
+.llm-button-img {
+    position: relative;
+    display: block;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin: 3px;
+    background: white;
+    box-shadow: 0 0 10px rgba(0,0,0,0.3);
+}
+
+@keyframes rotate {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 
+import AssistantButton from "$lib/assistant-button.svelte";
 import LlmModal from "$lib/llm/LLMModal.svelte";
 
 let displayLLM: boolean = $state(false);
 
 </script>
 
-<button onclick={() => displayLLM = true}>Show LLM</button>
 
 {#if displayLLM}
     <LlmModal close={async () => displayLLM = false}/>
+{:else}
+    <AssistantButton onclick={() => displayLLM = true}/>
 {/if}
