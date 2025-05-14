7b7c31ea16cb58bdd8bfdd38c6a4bb7f6d38e5f2
Nathan Cassereau <nathan@cassereau.fr>
2025-05-14 13:21:24 +0000

    Temporary fix while ollama is called in the browser so that it can build. (will be replaced with server-side)

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,43 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
-	test: {
-		workspace: [
-			{
-				extends: './vite.config.ts',
-				plugins: [svelteTesting()],
-
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      // Force Ollama to use its browser version instead of Node.js version
+      'ollama': 'ollama/dist/browser.mjs'
+    }
+  },
+  optimizeDeps: {
+    exclude: ['ollama']
+  },
+  ssr: {
+    // Add Node.js modules that should be treated as external in SSR build
+    noExternal: ['ollama']
+  },
+  test: {
+    workspace: [
+      {
+        extends: './vite.config.ts',
+        plugins: [svelteTesting()],
+        test: {
+          name: 'client',
+          environment: 'jsdom',
+          clearMocks: true,
+          include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+          exclude: ['src/lib/server/**'],
+          setupFiles: ['./vitest-setup-client.ts']
+        }
+      },
+      {
+        extends: './vite.config.ts',
+        test: {
+          name: 'server',
+          environment: 'node',
+          include: ['src/**/*.{test,spec}.{js,ts}'],
+          exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+        }
+      }
+    ]
+  }
 });
